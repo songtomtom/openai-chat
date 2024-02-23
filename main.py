4507697f71d9034d4a7517d5491b76bdea47aceb
songtomtom <celvincolvumn@gmail.com>
2024-02-23 16:48:50 +0900

    깃깃

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# 샘플 Python 스크립트입니다.
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
-# ⌃R을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 ⇧ 두 번을(를) 누릅니다.
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+org_id = os.getenv("OPENAI_ORG_ID")
 
+client = OpenAI(api_key=api_key, organization=org_id)
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 ⌘F8을(를) 누릅니다.
+with open("system_message.txt", "r") as file:
+    system_message = file.read().strip()
 
+with open("user_message.txt", "r") as file:
+    user_message = file.read().strip()
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+response = client.chat.completions.create(
+    model="gpt-4-turbo-preview",
+    messages=[
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": user_message}
+    ],
+    temperature=0.0,
+)
 
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+print(response.choices[0].message.content)
